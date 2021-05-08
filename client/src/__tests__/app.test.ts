5a8e5205535da3d9b5278cf63d479cfac2d56df1
Matthew M-B <matthewmbemail@gmail.com>
2021-05-08 17:42:18 -0400

    add page structure and router (#32)
    * add page structure and router
    
    * pages and button

--- client/src/__tests__/app.test.ts
+++
@@ -1,8 +0,0 @@
-import {render} from '@testing-library/svelte';
-import App from '../App.svelte';
-
-test('should render the text hello world!', () => {
-  const results = render(App, {props: {name: 'hello world'}});
-
-  expect(() => results.getByText('hello world')).not.toThrow();
-});
---
+++ client/src/pages/Paint/index.ts
@@ -0,0 +1,1 @@
+export {default as Paint} from './Paint.svelte';
