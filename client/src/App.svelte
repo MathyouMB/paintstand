0ca8deb59077364b32eb1a961d4285d71b058ad4
Matthew M-B <matthewmbemail@gmail.com>
2021-05-08 18:40:00 -0400

    graphql svelte setup (#33)
    * graphql svelte setup
    
    * rubocop

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,33 @@
 <script>
   import Router from 'svelte-spa-router';
+  import {ApolloClient, InMemoryCache} from '@apollo/client';
+  import {createHttpLink} from 'apollo-link-http';
+  import {setClient} from 'svelte-apollo';
+
   import {Home, Image, NotFound, Paint} from './pages';
+
   const routes = {
     '/': Home,
     '/image': Image,
     '/paint': Paint,
     '*': NotFound
   };
+
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3000/graphql',
+    headers: {
+      'Content-Type': 'application/json',
+      Authentication:
+        'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6Mn0.EWcKepSTEm3T0zCyisld8FkytXpLSQ094WWZy-Fiqr4'
+    }
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+
+  setClient(client);
 </script>
 
 <Router {routes} />
--- client/src/pages/Home/Home.svelte
+++ client/src/pages/Home/Home.svelte
@@ -1,6 +1,26 @@
 <script>
-    import { Button } from '../../components'
+  import {query} from 'svelte-apollo';
+  import {gql} from '@apollo/client';
+
+  import {Button} from '../../components';
+  import {IMAGES_LISTED} from '../../data';
+
+  const images = query(IMAGES_LISTED, {
+    variables: {page: 1, limit: 24}
+  });
 </script>
 
 <h1>Home</h1>
-<Button label="Create"/>
+<Button label="Create" />
+
+<ul>
+  {#if $images.loading}
+    <li>Loading...</li>
+  {:else if $images.error}
+    <li>ERROR: {$images.error.message}</li>
+  {:else}
+    {#each $images.data.imageListed.collection as image (image.id)}
+      <li>{image.title}</li>
+    {/each}
+  {/if}
+</ul>
