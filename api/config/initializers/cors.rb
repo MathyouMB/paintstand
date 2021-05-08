372f8ca10cf42709720de8b4d5e2a62b87415300
MathyouMB <matthewmacraebovell@bell.net>
2021-05-08 18:37:33 -0400

    rubocop

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -5,10 +5,10 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins '*'
-    resource '*',
+    origins "*"
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
